400bad9105ea6e83606bf99d20e3efca2c8c4add
Brian Njuguna <142185778+001kenji@users.noreply.github.com>
2025-06-24 21:23:53 +0300

    fixed building issues

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-//import istanbul from 'vite-plugin-istanbul';
 
-// export default defineConfig({
-//   build: {
-//     manifest: true,
-//     commonjsOptions: { transformMixedEsModules: true }
-//   },
-//   base: process.env.mode === "production" ? "/static/" : "/",
-//   root: "./",
-//   plugins: [
-//     tailwindcss(),
-//     react(),    
-//   ],
-//   server: {
-//     host: '192.168.100.52', //"192.168.56.1", // Use your Wi‑Fi IP or use "0.0.0.0" (or true) to listen on all interfaces
-//     // Optional: define the port if needed
-//     port: 5173,
-//     cors: {
-//       origin: [process.env.VITE_APP_API_URL],
-//       credentials: true
-//     }
-//   }
-// })
 
 export default defineConfig({
   build: {
@@ -37,12 +15,6 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),    
-    ],
-    server : {
-      host: '0.0.0.0', //"192.168.56.1", // Use your Wi‑Fi IP or use "0.0.0.0" (or true) to listen on all interfaces
-      cors : {
-        origin : [process.env.VITE_APP_API_URL],
-        credentials : true
-      }
-    }
+    ]
+    
 })
